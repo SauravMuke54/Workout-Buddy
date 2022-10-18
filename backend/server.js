4e323c9f7c91a6f84f6782fe14a90b288dfbedcd
SauravMuke54 <saurav54muke@gmail.com>
2022-10-18 15:59:11 +0530

    Main

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://sam:sam@workout.gwijt.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
